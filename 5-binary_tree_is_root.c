f3461d6fb95558890195e273bfd8333e030b3552
shaco-star <sherifm846@gmail.com>
2023-08-03 13:42:52 -0400

    commit

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,7 +8,7 @@
  * Return: 1 is node is root and 0 if not or NULL
 */
 
-int binary_tree_is_root(binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
